e0fae4cf49b8e5899b55002ea914bfed87fb4785
alexsinfarosa <as898@cornell.edu>
2018-11-13 21:48:24 -0500

    Updates

---
+++ precache-manifest.8ff075435dc4ab4989f148d1a5d2de49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df194b5e8022543056a3",
+    "url": "/climod-hr-observations/static/js/runtime~main.df194b5e.js"
+  },
+  {
+    "revision": "1118db269bdae255e7fe",
+    "url": "/climod-hr-observations/static/js/main.1118db26.chunk.js"
+  },
+  {
+    "revision": "34b8bb816d10ea31fc5e",
+    "url": "/climod-hr-observations/static/js/1.34b8bb81.chunk.js"
+  },
+  {
+    "revision": "1118db269bdae255e7fe",
+    "url": "/climod-hr-observations/static/css/main.2e5367a4.chunk.css"
+  },
+  {
+    "revision": "ea4326e8650217c452f5c87ebf06131b",
+    "url": "/climod-hr-observations/index.html"
+  }
+];
